bd599dac83a5d5e5061b0f9fc593b378d5022956
saurabh garkoti <saurabhgarkotiii@gmail.com>
2025-10-20 18:56:30 +0530

    feat: add cardlist component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import { AppAreaChart } from '@/components/AppAreaChart'
 import { AppBarChart } from '@/components/AppBarChart'
 import { AppPieChart } from '@/components/AppPieChart'
+import CardList from '@/components/CardList'
 import dbconnect from '@/lib/dbconnect'
 import React from 'react'
 
@@ -15,11 +16,11 @@ function Home() {
     <div className='bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2'>
       <AppBarChart/>
     </div>
-    <div className='bg-primary-foreground p-4 rounded-lg'>Test</div>
+    <div className='bg-primary-foreground p-4 rounded-lg'><CardList title='Latest Transactions'/></div>
     <div className='bg-primary-foreground p-4 rounded-lg'><AppPieChart/></div>
     <div className='bg-primary-foreground p-4 rounded-lg'>Test</div>
     <div className='bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-2'><AppAreaChart/></div>
-    <div className='bg-primary-foreground p-4 rounded-lg'>Test</div>
+    <div className='bg-primary-foreground p-4 rounded-lg'><CardList title='Popular Content'/></div>
  
    </div>
   )
